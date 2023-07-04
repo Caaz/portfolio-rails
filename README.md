bdbc755941901752743811ec8727d0fb6ae5c094
Daniel Caaz <dcaazy@gmail.com>
2023-07-03 18:05:44 -0700

    Basic landing page mostly designed!

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Getting Started
 Run the project with
 `docker compose up`
+
+## Random notes
+First request after restarting takes a while, this is normal, probably.
--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,1 +1,19 @@
 //= require uikit/dist/js/uikit
+//= require uikit/dist/js/uikit-icons
+
+
+
+window.addEventListener("load", (event) => {
+    const canvas = document.getElementById("hero-canvas");
+    if (canvas === null) return;
+    const ctx = canvas.getContext("2d");
+    window.setInterval(() => {
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#6527BE";
+        if(canvas.height !== canvas.offsetHeight || canvas.width !== canvas.offsetWidth) {
+            canvas.width = canvas.offsetWidth;
+            canvas.height = canvas.offsetHeight;
+        }
+        ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 10, 10);
+    }, 1000 / 30);
+});
