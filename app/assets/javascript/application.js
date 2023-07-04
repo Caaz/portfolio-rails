736015477fdceb185c8c1f0c0a7bda31e42146f3
Daniel Caaz <dcaazy@gmail.com>
2023-07-03 18:47:59 -0700

    Wee wooo here's some more nonsense

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,19 +1,4 @@
 //= require uikit/dist/js/uikit
 //= require uikit/dist/js/uikit-icons
+//= require_tree .
 
-
-
-window.addEventListener("load", (event) => {
-    const canvas = document.getElementById("hero-canvas");
-    if (canvas === null) return;
-    const ctx = canvas.getContext("2d");
-    window.setInterval(() => {
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "#6527BE";
-        if(canvas.height !== canvas.offsetHeight || canvas.width !== canvas.offsetWidth) {
-            canvas.width = canvas.offsetWidth;
-            canvas.height = canvas.offsetHeight;
-        }
-        ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 10, 10);
-    }, 1000 / 30);
-});
---
+++ app/assets/javascript/home.js
@@ -0,0 +1,19 @@
+
+window.addEventListener("load", (event) => {
+    // Set up Canvas
+    const canvas = document.getElementById("hero-canvas");
+    if (canvas === null) return;
+    const ctx = canvas.getContext("2d");
+    window.setInterval(() => {
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#6527BE";
+        if(canvas.height !== canvas.offsetHeight || canvas.width !== canvas.offsetWidth) {
+            canvas.width = canvas.offsetWidth;
+            canvas.height = canvas.offsetHeight;
+        }
+        ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 10, 10);
+    }, 1000 / 30);
+    // Randomize tagline
+    const taglines = document.querySelectorAll("#hero p");
+    taglines[Math.floor(Math.random()*taglines.length)].className = ""
+});
