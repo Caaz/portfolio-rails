ef07fd4ac2df8b82914a2997c9f762bcd15234c6
Daniel Caaz <dcaazy@gmail.com>
2023-07-21 15:43:52 -0700

    Spinning cube! Good enough for now.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,35 +6,63 @@ UIkit.use(Icons);
 window.addEventListener("load", (event) => {
     // Set up Canvas
     const canvas = document.getElementById("hero-canvas");
-    const noise2D = createNoise2D();
-    const particles = []
-    const particle_limit = 100;
-    const noise_zoom = 10;
+    let angle = 0;
+    
+    // Define callback (which will be called 30 frames a second due to the Artist class)
     const art = (artist) => {
-        const {ctx, canvas, frame} = artist;
-        ctx.fillStyle = "#6527BE";
-        const center = {x:canvas.width/2, y:canvas.height/2};
-        if (particles.length < particle_limit) {
-            
-            particles.push({
-                x: center.x + noise2D(Math.random(), Math.random()) * 10,
-                y: center.y + noise2D(Math.random(), Math.random()) * 10,
-                life:particle_limit
-            })
+        const {ctx, canvas} = artist
+        ctx.strokeStyle = "#6527BE";
+        ctx.lineWidth = 5;
+        ctx.lineJoin = "round";
+        const centerX = canvas.width / 2;
+        const centerY = canvas.height / 2;
+        // Clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Set rotation angle
+        angle += 0.01;
+        
+        // Draw cube
+        const size = 1;
+        const vertices = [
+            { x: -size, y: -size, z: -size },
+            { x: size, y: -size, z: -size },
+            { x: size, y: size, z: -size },
+            { x: -size, y: size, z: -size },
+            { x: -size, y: -size, z: size },
+            { x: size, y: -size, z: size },
+            { x: size, y: size, z: size },
+            { x: -size, y: size, z: size }
+        ];
+        const projection = [];
+        for (let i = 0; i < vertices.length; i++) {
+            const vertex = vertices[i];
+            const y = vertex.x;
+            const x = vertex.y * Math.cos(angle) - vertex.z * Math.sin(angle);
+            const z = vertex.y * Math.sin(angle) + vertex.z * Math.cos(angle) + size * 4;
+            const d = 1 / (z / 300);
+            const projectedX = x * d + centerX;
+            const projectedY = y * d + centerY;
+            projection.push({ x: projectedX, y: projectedY });
         }
-        particles.forEach(particle => {
-            var noise = noise2D(particle.x/noise_zoom, (particle.y+frame/10)/noise_zoom)
-            particle.y += -2 + noise * 2 * Math.random();
-            particle.x += 0 + noise * 5 * Math.random();
-            particle.life -= 1;
-
-            ctx.beginPath();
-            ctx.arc(particle.x, particle.y, 2, 0, 2 * Math.PI);
-            ctx.fill();
-        })
-        while(particles[0].life <= 0) {
-            particles.shift()
-        }
+        ctx.beginPath();
+        ctx.moveTo(projection[0].x, projection[0].y);
+        ctx.lineTo(projection[1].x, projection[1].y);
+        ctx.lineTo(projection[2].x, projection[2].y);
+        ctx.lineTo(projection[3].x, projection[3].y);
+        ctx.lineTo(projection[0].x, projection[0].y);
+        ctx.lineTo(projection[4].x, projection[4].y);
+        ctx.lineTo(projection[5].x, projection[5].y);
+        ctx.lineTo(projection[6].x, projection[6].y);
+        ctx.lineTo(projection[7].x, projection[7].y);
+        ctx.lineTo(projection[4].x, projection[4].y);
+        ctx.moveTo(projection[1].x, projection[1].y);
+        ctx.lineTo(projection[5].x, projection[5].y);
+        ctx.moveTo(projection[2].x, projection[2].y);
+        ctx.lineTo(projection[6].x, projection[6].y);
+        ctx.moveTo(projection[3].x, projection[3].y);
+        ctx.lineTo(projection[7].x, projection[7].y);
+        ctx.stroke();
     };
     const artist = new Artist(canvas, art);
     artist.begin()
