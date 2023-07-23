f18a96fdbaf21fa7551638255d2d213c74ded219
Daniel Caaz <dcaazy@gmail.com>
2023-07-23 14:03:05 -0700

    Basic implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :set_opengraph_values
+    protected
+    def set_opengraph_values
+        @og = {
+            title: "Portfolio",
+            type: :website,
+            image: helpers.asset_url("social.svg"),
+            url: request.original_url
+        }
+    end
 end
