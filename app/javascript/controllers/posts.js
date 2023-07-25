2abb88f38ef7386b6b878e069fbaaba1d7f5acf3
Daniel Caaz <dcaazy@gmail.com>
2023-07-25 14:30:03 -0700

    Fix load order.

--- app/javascript/controllers/posts.js
+++ app/javascript/controllers/posts.js
@@ -1,31 +1,36 @@
 import Rails from "@rails/ujs"
 import UIkit from "uikit"
 
+const BOTTOM = "#bottom-of-page"
+const POST_CONTAINER = "#post-index"
+
 export default {
     index: _ => {
         var oldest = new Date(Date.now()).toISOString()
-        UIkit.util.on('#post-index', 'inview', event => {
+        UIkit.util.on(POST_CONTAINER, 'inview', event => {
             Rails.$("title")[0].innerHTML = event.target.dataset.title
         });
-        UIkit.util.on('#bottom-of-page', 'inview', _ => {
+        UIkit.util.on(BOTTOM, 'inview', _ => {
             Rails.ajax({
                 url: "/posts.json",
                 data: new URLSearchParams({'before': oldest}).toString(),
                 type: "get",
                 success: data => { 
                     console.log(data)
-                    Rails.$("#post-index")[0].innerHTML += data.html
+                    Rails.$(POST_CONTAINER)[0].innerHTML += data.html
                     oldest = data.oldest
                     if(data.count != data.limit) {
-                        Rails.$("#bottom-of-page")[0].remove()
+                        Rails.$(BOTTOM)[0].remove()
                     }
                 },
                 error: err => {
                     console.error(err)
-                    Rails.$("#bottom-of-page")[0].remove()
+                    Rails.$(BOTTOM)[0].remove()
                     Rails.$("#fetch-error")[0].className = ""
                 }
             })
         });
+
+        UIkit.scrollspy(Rails.$(BOTTOM)[0])
     }
 }
